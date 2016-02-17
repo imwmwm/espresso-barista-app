4b7b7894f49598a4fa226966489456aa4a3ae1b2
Kenny Wong <wong@box.com>
2016-02-17 13:18:24 -0800

    fixed topbar

--- index.php
+++ index.php
@@ -1,13 +1,36 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Bootstrap 101 Template</title>
+    <title>Barista</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <!-- Navigation Bar -->
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container-fluid">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="#">Barista</a>
+          <div class="nav-collapse collapse">
+            <p class="navbar-text pull-right">
+              Logged in as <a href="#" class="navbar-link">Username</a>
+            </p>
+            <ul class="nav">
+              <li class="active"><a href="#">Orders</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+    <!-- End of Navigation Bar -->
+
+
     <script src="http://code.jquery.com/jquery.js"></script>
     <script src="/vendor/bootstrap/js/bootstrap.min.js"></script>
   </body>
